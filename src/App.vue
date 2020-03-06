3a0141ddbb5b252931b3bf77a030ccdb2d566f21
Study(ByoungChul) <skyksit@gmail.com>
2020-03-06 16:44:13 +0900

    BLD:Add environment variable

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="todo">
+      <b-navbar type="dark" variant="dark">
+        <b-navbar-nav>
+          <b-nav-item router-link to="/">Home</b-nav-item>
+          <b-nav-item router-link to="/users">Users</b-nav-item> |
+          <b-nav-item router-link to="/about">About</b-nav-item>
+        </b-navbar-nav>
+      </b-navbar>
     </div>
     <router-view />
   </div>
@@ -15,18 +20,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/views/Users.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="users">
+    <h1>This is an Users page</h1>
+    <Users />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Users from "@/components/Users.vue";
+
+export default {
+  name: "userlist",
+  components: {
+    Users
+  }
+};
+</script>
